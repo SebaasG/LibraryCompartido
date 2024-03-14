374476376886a7d1bbd7d8288b1b717c8ac64b62
Sebastian <miguel.poveda1220@gmail.com>
2024-03-14 15:47:09 -0500

    funcionando el modal pero sintraer datos

--- public/js/bookIndex.js
+++ public/js/bookIndex.js
@@ -1,55 +1,64 @@
+let contador = 0;
+
 document.addEventListener('DOMContentLoaded', function () {
-  alert('hola')
-  getBooks();
-    
+
+    getBooks();
+
 });
 
-// async function getBooks() {
-//   try {
-//       const response = await fetch('http://localhost:3000/book/all'); // Corrección en la URL, 'localHost' debería ser 'localhost'
-//       const datos = await response.json();
-      
-//       datos.forEach(dato => { // 'datos.array.forEach' debería ser simplemente 'datos.forEach'
-//           let div = document.createElement('div');
-//           div.innerHTML = `<div class="cardBookContainer card align-content-center fs-5" style="width: 15rem;">
-//               <img src="${dato.postbook}" class="card-img-top" alt="${dato.nameBook}">
-//               <div class="card-body">
-//                   <p class="card-text justify-content-center ">${dato.nameBook}</p>
-//               </div>
-//           </div>`;
-//           document.getElementById('containerBook').appendChild(div);
-//       });
-
-//       console.log(datos); // Este console.log p uede ser útil para depurar si la respuesta se ha obtenido correctamente
-//   } catch (error) {
-//       console.error('Error al obtener los libros:', error); // Manejo de errores
-//   }
-// }
 async function getBooks() {
-  try {
-      const response = await fetch('http://localhost:3000/book/all');
-      const datos = await response.json();
+    try {
+        const response = await fetch('http://localhost:3000/book/all');
+        const datos = await response.json();
 
-      const container = document.getElementById('containerBook');
-      container.innerHTML = ''; // Limpiamos el contenido anterior antes de agregar nuevos elementos
+        const container = document.getElementById('containerBook');
+        container.innerHTML = ''; // Limpiamos el contenido anterior antes de agregar nuevos elementos
 
-      datos.forEach(dato => {
-          let column = document.createElement('div');
-          column.classList.add('col-lg-4', 'col-md-6', 'mb-4'); // Aplicar clases de Bootstrap directamente
-          column.innerHTML = `<div class="cardBookContainer card align-content-center fs-5" style="width: 100%;">
-          <img src="${dato.postbook}" class="card-img-top" alt="Imagen de libro" onerror="this.onerror=null; this.src='https://www.procinal.com/uploads/PELICULAS/Img_movies/Img_360x500/360%20X%20500oppenheimer.jpg';">
+        datos.forEach(dato => {
+            contador = contador + 1
+            console.log(contador)
+            let column = document.createElement('div');
+            column.classList.add('col-lg-4', 'col-md-6', 'mb-4'); // Aplicar clases de Bootstrap directamente
+            column.innerHTML = `<div id="tarjet${contador}" class="cardBookContainer card align-content-center fs-5 "data-bs-toggle="modal" data-bs-target="#miModal" style="width: 100%; ">
+            <img src="${dato.postbook}" class="card-img-top" alt="Imagen de libro" onerror="this.onerror=null; this.src='https://www.movienewz.com/wp-content/uploads/2014/07/poster-holder.jpg';">
+            <div class="card-body">
+            <p class="card-text justify-content-center ">${dato.nameBook}<br><strong> ${dato.authbook}</strong></p>
+            </div>
+            </div>`;
+            container.appendChild(column);
+            document.querySelector('#tarjet' + contador).addEventListener('click', function () {
+                // aquí puedes ejecutar una función cuando se hace clic en la tarjeta
+                localStorage.setItem('idBookUser', dato.id)
 
-              <div class="card-body">
-                  <p class="card-text justify-content-center ">${dato.nameBook}</p>
-              </div>
-          </div>`;
-          container.appendChild(column);
-      });
+                // llenarModal(datos.id)
+            });
+        });
 
-      console.log(datos);
-  } catch (error) {
-      console.error('Error al obtener los libros:', error);
-  }
+
+        async function llenarModal(idBook) {
+            const response = await fetch('http://localHost:1234/task/doc/' + idBook);
+            const datos = await response.json();
+            const cate = validarCate(datos[0].cateTask);
+            const formDate = newDate(datos[0].dateStart, true);
+            const states = state(datos[0].stateTask);
+
+            document.getElementById('modalTitle').innerText = datos[0].nameTask;
+
+            document.getElementById('modalContent').innerHTML = `
+                <label  class="col-form-label">Description task:</label> 
+                <p> ${datos[0].descTask}</p>
+                <label  class="col-form-label">Date:</label> 
+                <p> ${formDate}</p>
+                <label  class="col-form-label">State:</label> 
+                <p> ${states}</p>
+                <label  class="col-form-label">Category:</label> 
+                <p> ${cate}</p> `;
+        }
+
+        console.log(datos);
+    } catch (error) {
+        console.error('Error al obtener los libros:', error);
+    }
 }
 
 
