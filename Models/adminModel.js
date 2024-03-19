55d8de06b6ae4bfdd8b3bcd515a3f3514da9618d
Andrey <andreyvr07@gmail.com>
2024-03-19 09:42:58 -0500

    Obtener datos del buscador con filtro

--- Models/adminModel.js
+++ Models/adminModel.js
@@ -1,45 +1,95 @@
 import { createConnection } from "../Databases/config.js";
-const connection =await createConnection();
+const connection = await createConnection();
 export class adminMdl {
+  // Modelo para obtener todos los libros de la bd.
+  static async getBooks() {
+    try {
+      const [sql] = await connection.query(
+        " SELECT BIN_TO_UUID(idBook) as idBook,nameBook, amountBook, genBook,  yearbook, authbook, disableBook, postbook, sumBook FROM book"
+      );
+      return sql;
+    } catch (err) {
+      console.log("Hubo un error en el modelo.", err);
+      throw err;
+    }
+  }
+  // Modelo para obtener un libro por id.
+  static async getBookById(idBook) {
+    try {
+      const [sql] = await connection.query(
+        "SELECT BIN_TO_UUID(idBook) as id,nameBook, amountBook, genBook, sumBook, yearbook, authbook, postbook FROM book WHERE BIN_TO_UUID(idBook) = ?",
+        [idBook]
+      );
+      return sql;
+    } catch (err) {
+      console.log("Hubo un error en la busqueda del libro");
+      throw err;
+    }
+  }
+  // Actualizar libros
+  static async updateBooks({ body }) {
+    try {
+      const {
+        idBook,
+        nameBook,
+        yearbook,
+        amountBook,
+        postbook,
+        sumBook,
+        authbook,
+        genBook,
+        disableBook,
+      } = body;
+      console.log(`id: ${idBook}, nameBook = ${nameBook}`);
 
-    // Modelo para obtener todos los libros de la bd.
-    static async getBooks (){
-        try{
-        const [sql] = await connection.query(' SELECT BIN_TO_UUID(idBook) as idBook,nameBook, amountBook, genBook,  yearbook, authbook, disableBook, postbook, sumBook FROM book')
-        return sql;
-        }catch(err){
-            console.log('Hubo un error en el modelo.', err);
-            throw err;
-        }
-    }   
-    // Modelo para obtener un libro por id.
-    static async getBookById(idBook){
-        try{
-            const [sql] = await connection.query('SELECT BIN_TO_UUID(idBook) as id,nameBook, amountBook, genBook, sumBook, yearbook, authbook, postbook FROM book WHERE BIN_TO_UUID(idBook) = ?',[idBook]);
-            return sql;
-        }catch(err){
-            console.log('Hubo un error en la busqueda del libro');
-            throw err;
-        }
+      const sql = await connection.query(
+        "UPDATE book SET nameBook = ?, yearbook = ?,amountBook = ?,postbook=?, sumBook = ?, authbook = ?, genBook = ?, disableBook = ?   WHERE BIN_TO_UUID(idBook) = ?",
+        [
+          nameBook,
+          yearbook,
+          amountBook,
+          postbook,
+          sumBook,
+          authbook,
+          genBook,
+          disableBook,
+          idBook,
+        ]
+      );
+      return sql;
+    } catch (err) {
+      console.log("Hubo un error al actualizar los libros.");
+      throw err;
     }
-    // Actualizar libros
-    static async updateBooks({body}){
-        try{
-            const {idBook, nameBook, yearbook, amountBook, postbook, sumBook, authbook, genBook, disableBook} = body;
-            console.log(`id: ${idBook}, nameBook = ${nameBook}`)
-          
-            const sql = await connection.query('UPDATE book SET nameBook = ?, yearbook = ?,amountBook = ?,postbook=?, sumBook = ?, authbook = ?, genBook = ?, disableBook = ?   WHERE BIN_TO_UUID(idBook) = ?', [nameBook,yearbook,amountBook,postbook, sumBook, authbook, genBook,disableBook, idBook ]);
-            return sql;
-        }catch(err){
-            console.log('Hubo un error al actualizar los libros.')
-            throw err;
-        }
+  }
+  // Crear Libros
+  static async createBooks({ body }) {
+    const {
+      nameBook,
+      yearbook,
+      amountBook,
+      postbook,
+      sumBook,
+      authbook,
+      genBook,
+    } = body;
+    const sql = await connection.query(
+      "INSERT INTO book (nameBook, amountBook, genBook, sumBook, yearbook, authbook, postbook, disableBook) VALUES (?,?,?,?,?,?,?,1)",
+      [nameBook, amountBook, genBook, sumBook, yearbook, authbook, postbook]
+    );
+    return sql;
+  }
+  //Buscar Libros
+  static async shearchBooks(filter, data) {
+    try {
+      console.log("Si se esta mandando algo al modelo", filter, data);
+      const sql = await connection.query(
+        "SELECT * FROM book WHERE " + filter + " LIKE ?", [`%${data}%`]
+      );
+      return sql;
+    } catch (err) {
+      console.log("Hubo un error en el buscador de libros.");
+      throw err;
     }
-    // Crear Libros
-    static async createBooks({body}){
-            const { nameBook, yearbook, amountBook, postbook, sumBook, authbook, genBook } = body;
-            console.log('Si se esta pasando algo')
-            const sql = await connection.query('INSERT INTO book (nameBook, amountBook, genBook, sumBook, yearbook, authbook, postbook, disableBook) VALUES (?,?,?,?,?,?,?,1)',[nameBook, amountBook, genBook, sumBook, yearbook, authbook, postbook])
-            return sql;
-        }
+  }
 }
---
+++ public/js/shearch.js
@@ -0,0 +1,6 @@
+const inptShearch = document.getElementById('inputSearchBook').value;
+const shearchBy = document.getElementById('shearchBoook').value;
+
+async function shearchBy(){
+    await fetch ('')
+}
