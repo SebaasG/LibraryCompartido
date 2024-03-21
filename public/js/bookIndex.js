ad41c077fda7ccb452879a713ec2fd0547671a76
Sebastian <miguel.poveda1220@gmail.com>
2024-03-21 13:41:03 -0500

    Filter funcionando, + reducción de código

--- public/js/bookIndex.js
+++ public/js/bookIndex.js
@@ -4,18 +4,30 @@ const nameint = document.getElementById('searchNameBook');
 const cleanFilter = document.getElementById('cleanFiltrer');
 
 async function getGender() {
-    await fetchDataAndPopulateFilter('http://localhost:3000/book/index', 'GenderFilter', 'nameGen', 'ButtonGen', 'GenderBook');
+    await fetchFilter('http://localhost:3000/book/index', 'GenderFilter', 'nameGen', 'ButtonGen', 'GenderBook');
 }
 
 async function getAge() {
-    await fetchDataAndPopulateFilter('http://localHost:3000/book/age', 'AgesFilter', 'yearbook', 'Buttonage', 'AgeBook');
+    await fetchFilter('http://localHost:3000/book/age', 'AgesFilter', 'yearbook', 'Buttonage', 'AgeBook');
 }
 
-async function getAuthorbook() {
-    await fetchDataAndPopulateFilter('http://localHost:3000/book/author', 'AuthFilter', 'authbook', 'ButtonAuth', 'AuthBook');
+async function getAuthor() {
+    await fetchFilter('http://localHost:3000/book/author', 'AuthFilter', 'authbook', 'ButtonAuth', 'AuthBook');
 }
 
-async function fetchDataAndPopulateFilter(url, containerId, dataProperty, buttonIdPrefix, localStorageKey) {
+async function getByGender(nameGender) {
+    await getDataForFilter('http://localHost:3000/book/index/gen/' + nameGender, 'containerBook', 'datos');
+}
+
+async function getByAuthor(authbook) {
+    await getDataForFilter('http://localhost:3000/book/index/author/' + authbook, 'containerBook', 'datos');
+}
+
+async function getByAge(yearbook) {
+    await getDataForFilter('http://localhost:3000/book/index/year/' + yearbook, 'containerBook', 'datos');
+}
+
+async function fetchFilter(url, containerId, dataProperty, buttonIdPrefix, localStorageKey) {
     const response = await fetch(url);
     const datos = await response.json();
     const container = document.getElementById(containerId);
@@ -30,11 +42,11 @@ async function fetchDataAndPopulateFilter(url, containerId, dataProperty, button
         button.addEventListener('click', function () {
         
             if(containerId === 'GenderFilter'){
-                getGenderByName(dato[dataProperty]);
+                getByGender(dato[dataProperty]);
             }else if(containerId === 'AuthFilter'){
-                alert('para autor')
+                getByAuthor(dato[dataProperty]);
             }else{
-                alert('para año')
+                getByAge(dato[dataProperty]);
             }
             localStorage.setItem(localStorageKey, dato[dataProperty]);
             cambiarColor(buttonId);
@@ -57,17 +69,15 @@ function cambiarColor(id) {
     botonActual.classList.add("presionado");// Aplicar la clase 'presionado' solo al botón actual
 }
 
-async function getGenderByName(nameGender) {
-    const response = await fetch('http://localHost:3000/book/index/gen/' + nameGender);
+async function getDataForFilter(url, containerId, dataKey) {
+    const response = await fetch(url);
     const datos = await response.json();
-    console.log(datos)
-    const container = document.getElementById('containerBook');
+    const container = document.getElementById(containerId);
     container.innerHTML = '';
-    
-    datos.forEach(dato => {
-        contador = contador + 1
-        let column = document.createElement('div');
 
+    datos.forEach(dato => {
+        contador++;
+        const column = document.createElement('div');
         column.classList.add("justify-content-center", "col-lg-4", "col-md-6", "mb-4");
         column.innerHTML = `<div id="tarjet${contador}" class="cardBookContainer card align-content-center fs-5 "data-bs-toggle="modal" data-bs-target="#miModal" style="width: 100%; ">
         <img src="${dato.postbook}" class="card-img-top" alt="Imagen de libro" onerror="this.onerror=null; this.src='https://www.movienewz.com/wp-content/uploads/2014/07/poster-holder.jpg';">
@@ -77,33 +87,7 @@ async function getGenderByName(nameGender) {
         </div>`;
         container.appendChild(column);
         document.querySelector('#tarjet' + contador).addEventListener('click', function () {
-            // aquí puedes ejecutar una función cuando se hace clic en la tarjeta
-            localStorage.setItem('idBookUser', dato.id)
-            window.location.href = '../views/BookData.html';
-        });
-    });
-}
-
-async function getAuthor(authbook) {
-    const response = await fetch('http://localhost:3000/book/index/author/' + authbook);
-    const datos = await response.json();
-    const container = document.getElementById('containerBook');
-    container.innerHTML = '';
-    datos.forEach(dato => {
-        contador = contador + 1
-        let column = document.createElement('div');
-
-        column.classList.add("justify-content-center", "col-lg-4", "col-md-6", "mb-4");
-        column.innerHTML = `<div id="tarjet${contador}" class="cardBookContainer card align-content-center fs-5 "data-bs-toggle="modal" data-bs-target="#miModal" style="width: 100%; ">
-        <img src="${dato.postbook}" class="card-img-top" alt="Imagen de libro" onerror="this.onerror=null; this.src='https://www.movienewz.com/wp-content/uploads/2014/07/poster-holder.jpg';">
-        <div class="card-body">
-        <p class="card-text justify-content-center ">${dato.nameBook}<br><strong> ${dato.authbook}</strong></p>
-        </div>
-        </div>`;
-        container.appendChild(column);
-        document.querySelector('#tarjet' + contador).addEventListener('click', function () {
-            // aquí puedes ejecutar una función cuando se hace clic en la tarjeta
-            localStorage.setItem('idBookUser', dato.id)
+            localStorage.setItem('idBookUser', dato.id);
             window.location.href = '../views/BookData.html';
         });
     });
@@ -185,5 +169,12 @@ document.addEventListener('DOMContentLoaded', function () {
     getBooks();
     getGender();
     getAge();
-    getAuthorbook();
+    getAuthor();
 });
+
+const userBook = document.getElementById('btnUserBook');
+
+userBook.addEventListener('click', ()=>{
+    alert('hola mi bro ')
+    window.location.href = '../views/userData.html'
+})
