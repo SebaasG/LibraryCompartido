fc997a8734a3dabc17b97cc98add2ec8e05259cf
Andrey <andreyvr07@gmail.com>
2024-04-08 14:30:28 -0500

    intento de search Transac

--- Models/historyModel.js
+++ Models/historyModel.js
@@ -6,7 +6,7 @@ export class historyMDL{
     
     static async getDataTransac(page){
         try{    
-            page = (page - 1) *10
+            page = (page - 1) * 10
             const [[sql]] = await connection.execute('CALL getTransac(?)', [page]);
             const [countRegisters] = await connection.query('SELECT COUNT(*) AS registers FROM transac;');
             const count = Math.ceil(countRegisters[0].registers /10);
@@ -16,5 +16,8 @@ export class historyMDL{
             console.log(e);
             throw new Error;
         }
+    }
+    static async searchTransac(filterTransac, input){
+        
     }
 }
--- public/js/history.js
+++ public/js/history.js
@@ -1,13 +1,73 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  await getTrasacData(1);
+});
 
-document.addEventListener('DOMContentLoaded', async()=>{
-    await getTrasacData(1);
-})
+async function getTrasacData(page) {
+  const response = await fetch(
+    `http://localhost:3000/admin/books/history/${page}`
+  );
+  const dataResponse = await response.json();
+  const { data, tabs } = dataResponse;
 
- async function getTrasacData(page){
-
-    const response = await fetch(`http://localhost:3000/admin/books/history/${page}`)
-    const dataResponse = await response.json();
-    const {data, tabs} = dataResponse;
-
+  pagination(tabs);
+  renderData(data);
 }
 
+function pagination(tabs) {
+  const containerTabs = document.querySelector("#pagination");
+
+  containerTabs.innerHTML = "";
+  for (let i = 1; i <= tabs; i++) {
+    const li = document.createElement("li");
+    li.classList.add("page-item");
+
+    li.innerHTML = ` <a  id ="page${i}"  class="page-link" >${i}</a> `;
+    containerTabs.appendChild(li);
+
+    const page = document.querySelector(`#page${i}`);
+    page.addEventListener("click", async () => {
+      console.log("epa", i);
+      await getTrasacData(i);
+    });
+  }
+}
+
+function renderData(data) {
+  const bodyTable = document.querySelector("#body-table");
+  console.log(data);
+  bodyTable.innerHTML = "";
+
+  data.forEach((d) => {
+    const tr = document.createElement("tr");
+    let date = new Date(d.dateTrans);
+    let newDate = date.toLocaleDateString("es-ES");
+    let newHour = date.toLocaleTimeString("es-ES");
+    tr.innerHTML = `
+        <td class="" >${d.nameType} </td>
+        <td class="" >${d.nameUser} </td>
+        <td class="" >${d.nameBook} </td>
+        <td class="" >${newDate} </td>
+        <td class="" >${newHour} </td>
+
+        `;
+    bodyTable.appendChild(tr);
+  });
+}
+
+const inputSearch = document.querySelector("#inputSearch");
+inputSearch.addEventListener("input", async () => {
+  await searchTransac();
+});
+async function searchTransac() {
+  const inputSearch = document.querySelector("#inputSearch").value;
+  const selectTransac = document.querySelector("#selectTransac").value;
+  const tableBody = document.querySelector('#body-table');
+  tableBody.innerHTML = '';   
+
+  console.log(inputSearch)
+  console.log(selectTransac)
+  const response = fetch(``)
+
+
+
+}
